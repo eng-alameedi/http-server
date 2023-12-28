672477b8c3cbc7817c7593dc755a180d456ec26e
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-12-28 23:55:33 +0300

    add a header file, include the mean server functions

---
+++ src/include/Http_Server.h
@@ -0,0 +1,10 @@
+#ifndef _HTTP_SERVER_H_
+#define _HTTP_SERVER_H_
+
+//! Declare the Standard Library
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+//! code here
+
+#endif  //! _HTTP_SERVER_H_
