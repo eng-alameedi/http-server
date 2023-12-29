3e661d05522cebd8042d41c5a39fbf1eb231cdef
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-12-29 23:49:31 +0300

    add new functions to Http_Server.h file

--- src/include/Http_Server.h
+++ src/include/Http_Server.h
@@ -5,6 +5,13 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-//! code here
+//! Function to create the connection socket
+void create_socket();
+
+//! Function to bind to the connection socket
+void bind_socket();
+
+//! Function to listen to socket
+void listen_socket();
 
 #endif  //! _HTTP_SERVER_H_
